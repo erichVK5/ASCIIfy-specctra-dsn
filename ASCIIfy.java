f4162dc2efe0a8ebe0cd29f6430e1f6c1a25f488
erichVK5 <a1039181@gmail.com>
2022-04-24 05:58:26 +0930

    Update ASCIIfy.java

--- ASCIIfy.java
+++ ASCIIfy.java
@@ -1,3 +1,34 @@
+/*
+ *   Copyright (C) 2022 Erich S. Heinzle
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License at <http://www.gnu.org/licenses/> 
+ *   for more details.
+ *
+ * ASCIIfy.java
+ *
+ * A utility to sanitise specctra DSN files with non ASCII content for autorouting
+ * and simplify subsequent restoration of the non ASCII text in the autorouted .ses file.
+ *
+ * Usage:
+ *  java ASCIIfy filename.dsn
+ * 
+ * Outputs:
+ *  filename-ASCII.dsn
+ *  filename-ASCII.ses-deASCIIfy.sh
+ *
+ * Usage after autorouting:
+ *  /bin/sh filename-ASCII.ses-deASCIIfy.sh
+ *
+ */
+
 import java.io.File;
 import java.util.Scanner;
 import java.io.PrintWriter;
